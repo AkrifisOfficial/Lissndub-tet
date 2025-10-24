5ab8cd36900839219f072fb88ab194f31c5d8e69
Akrifstg <akrifisofficial@gmail.com>
2025-10-24 18:06:42 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js - ЗАМЕНИТЕ НА ВАШИ РЕАЛЬНЫЕ КЛЮЧИ
+const S3_CONFIG = {
+    endpoint: 'https://s3.ru1.storage.beget.cloud',
+    bucket: '24fe7486130a-dreamyvoice-space', // Ваш бакет
+    accessKeyId: 'HIBU211GJ55Y8GWD2W36', // Замените на реальный
+    secretAccessKey: 'MNoYWQmceDU5fkH2HHQ1hIzQrBDzX4u2yZUsULPx' // Замените на реальный
+};
